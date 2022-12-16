5783a12f4588fae067ca4381310edc8f5f8282c9
sihambyte <sihamargaw@gmail.com>
2022-12-15 18:42:35 -0800

    minor changes

--- UNO/public/javascripts/games/join.js
+++ UNO/public/javascripts/games/join.js
@@ -156,14 +156,11 @@ fetch(window.location.pathname, { method: "post" })
   .then((r) => r.json())
   .then(({ game_id }) => {
     socket.on(`game:${game_id}:player-joined`, ({ count, required_count }) => {
-      document.querySelector("span.current-count").innerHTML = count;
-
+      document.querySelector("span.current-count").innerHTML = `waiting for players ${count} of ${required_count}`;
+      //full, ready for start game
       if (count === required_count) {
-        document.querySelector("p#waiting").classList.add("hidden");
+        document.querySelector("#waiting").classList.add("hidden");
         document.querySelector("#game-table").classList.remove("hidden");
-        document
-          .querySelector("#game-table")
-          .classList.add(`player-count-${count}`);
       }
     });
 
